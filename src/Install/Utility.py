f4034ac451eb6b1864a6ba923f33cfc3ebfc0e7e
DBrianKimmel <d.briankimmel@gmail.com>
2015-10-13 18:06:06 -0400

    Initial Commit

---
+++ src/Install/Utility.py
@@ -0,0 +1,13 @@
+"""
+@name:      PyHouse_Install/src/Install/Utility.py
+@author:    D. Brian Kimmel
+@contact:   D.BrianKimmel@gmail.com
+@copyright: (c) 2015-2015 by D. Brian Kimmel
+@license:   MIT License
+@note:      Created on Oct 13, 2015
+@Summary:
+
+"""
+
+
+# ## END DBK
---
+++ src/Install/systemd.py
@@ -0,0 +1,19 @@
+"""
+@name:      PyHouse_Install/src/Install/systemd.py
+@author:    D. Brian Kimmel
+@contact:   D.BrianKimmel@gmail.com
+@copyright: (c) 2015-2015 by D. Brian Kimmel
+@license:   MIT License
+@note:      Created on Oct 13, 2015
+@Summary:
+
+"""
+
+def mkdir1(object):
+    """
+    mkdir -p /etc/systemd/system/getty@tty1.service.d/
+
+    cp autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
+    """
+
+# ## END DBK
