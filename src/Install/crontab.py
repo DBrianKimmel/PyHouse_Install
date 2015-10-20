0b0626e7a97092f304fcd6f73807f5ea13c44510
DBrianKimmel <d.briankimmel@gmail.com>
2015-10-20 12:17:02 -0400

    WIP

---
+++ src/Install/crontab.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+@name:      PyHouse_Install/src/Install/crontab.py
+@author:    D. Brian Kimmel
+@contact:   D.BrianKimmel@gmail.com
+@copyright: (c) 2015-2015 by D. Brian Kimmel
+@license:   MIT License
+@note:      Created on Oct 19, 2015
+@Summary:   Installs a crontab for a PyHouse Node
+
+Crontab to reboot once a week
+
+"""
+
+
+if __name__ == "__main__":
+    print('Running crontab.py ...')
+    l_boot = AutoStartOnBoot()
+    l_boot.detect_systemd()
+
+# ## END DBK
