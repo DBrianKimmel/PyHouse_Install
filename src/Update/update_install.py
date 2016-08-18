ea7bc911caa220cd86c5299f5b23e8c662a817c5
briank <briank@briank-Laptop-3>
2016-08-18 12:22:03 -0400

    WIP

--- src/Update/update_install.py
+++ src/Update/update_install.py
@@ -7,7 +7,11 @@
 @note:      Created on Oct 14, 2015
 @Summary:
 
+This is run as root by the shell script update_install which is run as bin/update_install
 """
+
+__updated__ = '2016-08-18'
+
 
 #  Import system type stuff
 import os
@@ -21,7 +25,8 @@ import stat
 HOME_DIR = '/home/pyhouse/'
 WORKSPACE_DIR = HOME_DIR + 'workspace/'
 HOME_BIN_DIR = HOME_DIR + 'bin/'
-INSTALL_DIR = WORKSPACE_DIR + 'PyHouse_Install/bin'
+INSTALL_DIR = WORKSPACE_DIR + 'PyHouse_Install/'
+INSTALL_BIN_DIR = INSTALL_DIR + 'bin/'
 
 
 class Utilities(object):
@@ -65,8 +70,8 @@ class Api(object):
         Utilities.MakeDir('bin', 'pyhouse')
         l_user = pwd.getpwnam('pyhouse')
 
-        for l_entry in os.listdir(INSTALL_DIR):
-            l_file = os.path.join(INSTALL_DIR, l_entry)
+        for l_entry in os.listdir(INSTALL_BIN_DIR):
+            l_file = os.path.join(INSTALL_BIN_DIR, l_entry)
             l_target = os.path.join(HOME_BIN_DIR, l_entry)
             shutil.copy(l_file, HOME_BIN_DIR)
             try:
@@ -78,11 +83,13 @@ class Api(object):
 
     def copy_pyhouse_service(self):
         """ Install in ~/.config/systemd/user/pyhouse.service
+        sudo systemctl daemon-reload
+
         """
         l_user = pwd.getpwnam('pyhouse')
         l_file = 'pyhouse.service'
         l_dir = HOME_DIR + '.config/systemd/user/'
-        l_src = HOME_DIR + 'workspace/PyHouse_Install/src/files/' + l_file
+        l_src = INSTALL_DIR + 'src/files/' + l_file
         l_dest = os.path.join(l_dir, l_file)
         if not os.path.isdir(l_dir):
             print('Creating a directory {}'.format(l_dir))
